e9173627e6d9bb0c88dee32b8a7e00af55344de2
Vinicius Dias de Oliveira Gardelli <gardelli123@gmail.com>
2017-09-11 13:33:58 -0300

    Added tmux plugin manager and some JS plugins to vim

--- tmux/install.bat
+++ tmux/install.bat
@@ -2,4 +2,5 @@
 set "SCRIPT_DIR=%~dp0"
 rem set "HOME_DIR=%HOMEDRIVE%%HOMEPATH%"
 
-mklink %HOME%\.tmux.conf %SCRIPT_DIR%\tmux.conf
+mklink    %HOME%\.tmux.conf %SCRIPT_DIR%\tmux.conf
+mklink /d %HOME%\.tmux      %SCRIPT_DIR%\tmuxdir
--- tmux/install.sh
+++ tmux/install.sh
@@ -3,3 +3,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -snf $SCRIPT_DIR/tmux.conf           $HOME/.tmux.conf
+ln -snf $SCRIPT_DIR/tmuxdir             $HOME/.tmux
