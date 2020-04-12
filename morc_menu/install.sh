2068fa15bb2c12e1c5017ef511296a48a0574677
thegards <gardelli123@gmail.com>
2020-04-12 13:50:49 -0300

    Adding OS checks for dmenu and morc_menu

--- morc_menu/install.sh
+++ morc_menu/install.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p ${HOME}/.config/morc_menu
+[ x"`uname`" != x"Linux" ] && echo "morc_menu only works on linux" && exit 0
+
+[ -d ~/.config/morc_menu ] || mkdir -p ~/.config/morc_menu
 
 ln -snf ${SCRIPT_DIR}/morc_menu.conf ${HOME}/.config/morc_menu/morc_menu_v1.conf
