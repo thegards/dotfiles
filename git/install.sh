0710d7e263574dad23b5e22fe7e5f87f2ecfdb2a
thegards <gardelli123@gmail.com>
2020-05-08 20:47:35 -0300

    Changed gitconfig install style to copy instead of link

--- git/install.sh
+++ git/install.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -snf ${SCRIPT_DIR}/common.gitconfig     ${HOME}/.gitconfig
+cp -f ${SCRIPT_DIR}/common.gitconfig       ${HOME}/.gitconfig
