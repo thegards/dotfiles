71385208900db196b217abe41fb2edf48a3bd456
thegards <3706493+thegards@users.noreply.github.com>
2022-03-31 10:00:13 -0300

    WIP

--- shell/install.sh
+++ shell/install.sh
@@ -4,5 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -snf ${SCRIPT_DIR}/aliases              ${HOME}/.bash_aliases
 ln -snf ${SCRIPT_DIR}/bash_common          ${HOME}/.bash_common
+ln -snf ${SCRIPT_DIR}/bash_functions       ${HOME}/.bash_functions
 ln -snf ${SCRIPT_DIR}/profile              ${HOME}/.bash_profile
 ln -snf ${SCRIPT_DIR}/bashrc               ${HOME}/.bashrc
