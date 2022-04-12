1558331bc75e9ad036bdd6dcc1091bb542f0263d
thegards <3706493+thegards@users.noreply.github.com>
2022-04-12 13:47:08 -0300

    WIP

--- X11/install.sh
+++ X11/install.sh
@@ -2,4 +2,12 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cpp -P  ${SCRIPT_DIR}/Xresources         > ${HOME}/.Xresources
+IS_UBUNTU=`uname -v | grep -Eio 'Ubuntu'`
+
+# If installing in Ubuntu boxes, preprocess Xresources because ubuntu stinks
+if [[ x$UNAME_V == x"Ubuntu" ]]
+then
+  cpp -P  ${SCRIPT_DIR}/Xresources         > ${HOME}/.Xresources
+else
+  ln -snf ${SCRIPT_DIR}/Xresources           ${HOME}/.Xresources
+fi
