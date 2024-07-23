6dc4431d168680a38f51a4daccd66c2386f7b454
Vinicius Gardelli <vinicius.gardelli@nubank.com.br>
2024-07-23 12:05:41 -0300

    neovim lua configuration + plugin replacements

---
+++ neovim/nvimdir_lua/after/plugin/completions.lua
@@ -0,0 +1,61 @@
+if not vim.g.loaded_my_vimrc then
+  return
+end
+if vim.g.loaded_my_completions_lua then
+  return
+end
+vim.g.loaded_my_completions_lua = true
+
+-- Set up nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+cmp.setup.filetype {
+  filetype = "clojure",
+  sources = {{name = "conjure"}}
+}
+
--- neovim/nvimdir_lua/lua/plugins/general-purpose.lua
+++ neovim/nvimdir_lua/lua/plugins/general-purpose.lua
@@ -8,13 +8,67 @@ return {
   { "tpope/vim-fugitive" }, 
   { "airblade/vim-gitgutter" }, 
   {
-    "luochen1990/rainbow",
+    "HiPhish/rainbow-delimiters.nvim",
     config = function()
-      vim.g.rainbow_active=true
+      local rainbow_delimiters = require("rainbow-delimiters")
+
+      require("rainbow-delimiters.setup").setup{
+        strategy = {
+          [''] = rainbow_delimiters.strategy['global'],
+          vim = rainbow_delimiters.strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+        },
+        priority = {
+          [''] = 110,
+          lua = 210,
+        },
+        highlight = {
+          'RainbowDelimiterRed',
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterGreen',
+          'RainbowDelimiterViolet',
+          'RainbowDelimiterCyan',
+        },
+      }
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    main = "nvim-treesitter.configs",
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      sync_install = false,
+      ensure_installed = {
+        "vimdoc",
+        "lua",
+        "clojure",
+        "bash",
+        "json",
+        "markdown",
+        "sql",
+        "yaml",
+        "scala"
+      }
+    },
+  },
+  { "tpope/vim-surround" }, 
+  {
+    "thegards/vim-dispatch",
+    branch = "wip/start-on-split-tmux-pane",
+    init = function()
+      vim.g.dispatch_no_maps = 1
     end,
   }, 
-  { "tpope/vim-surround" }, 
-  { "tpope/vim-dispatch" }, 
   {
     "nvim-lua/plenary.nvim",
     lazy = true,
