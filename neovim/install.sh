7609a3df541b468580be04b79211e744049aace7
thegards <3706493+thegards@users.noreply.github.com>
2024-09-07 22:33:35 -0300

    add new nvimdir_lua option to configs and update install script with a choice

--- neovim/install.sh
+++ neovim/install.sh
@@ -3,4 +3,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG_DIR=${XDG_CONFIG_HOME:-${HOME}/.config}
 
-ln -snf ${SCRIPT_DIR}/nvimdir                 ${CONFIG_DIR}/nvim
+config_type=$1
+case $1 in
+  "initvim"|"lua") config_type=$1;;
+  *) echo "Choose a config type: initvim|lua" && exit 1;;
+esac
+
+ln -snf ${SCRIPT_DIR}/nvimdir_${config_type}  ${CONFIG_DIR}/nvim
