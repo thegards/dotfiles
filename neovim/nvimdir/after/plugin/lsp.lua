0507ffeececc2685717884c6501655c4d0bbd9df
thegards <3706493+thegards@users.noreply.github.com>
2024-09-07 22:33:35 -0300

    add nvim dotfiles

---
+++ neovim/nvimdir/after/plugin/lsp.lua
@@ -0,0 +1,23 @@
+if not vim.g.loaded_my_vimrc then
+  return
+end
+
+require"my_utilities"
+
+if vim.g.loaded_my_lsp_lua then
+  return
+end
+vim.g.loaded_my_lsp_lua = true
+
+vim.keymap.set("n", "K",                   function() vim.lsp.buf.hover() end,             { desc = "Show documentation" })
+vim.keymap.set("n", "<c-]>",               function() vim.lsp.buf.definition() end,        { desc = "Go to definition" })
+vim.keymap.set("n", as_cmd("D"),           function() vim.lsp.buf.declaration() end,       { desc = "Go to declaration" })
+vim.keymap.set("n", as_cmd("t"),           function() vim.lsp.buf.type_definition() end,   { desc = "Go to type definition" })
+vim.keymap.set("n", as_cmd("r"),           function() vim.lsp.buf.references() end,        { desc = "Show symbol references" })
+vim.keymap.set("n", as_cmd("0"),           function() vim.lsp.buf.document_symbol() end,   { desc = "Go to document symbol" })
+vim.keymap.set("n", as_cmd("W"),           function() vim.lsp.buf.workspace_symbol() end,  { desc = "Go to workspace symbol" })
+vim.keymap.set("n", as_cmd("A"),           function() vim.lsp.buf.code_action() end,       { desc = "Execute code action" })
+vim.keymap.set("x", as_cmd("A"),           function() vim.lsp.buf.range_code_action() end, { desc = "Execute code action for selected range" })
+vim.keymap.set("n", as_refactor_cmd("rs"), function() vim.lsp.buf.rename() end,            { desc = "Rename symbol under cursor" })
+
+
---
+++ neovim/nvimdir/lua/my_utilities.lua
@@ -0,0 +1,32 @@
+if vim.g.my_utilities_loaded then
+  return
+end
+vim.g.my_utitities_loaded = true
+
+local global_search_prefix = '<leader>/'
+if vim.g.global_search_prefix then
+  global_search_prefix = vim.g.global_search_prefix
+end
+
+function as_search_cmd(command)
+  return global_search_prefix .. command
+end
+
+local command_prefix = '<localleader>g'
+if vim.g.clojure_bindings_prefix then
+  command_prefix = vim.g.lsp_bindings_prefix
+end
+
+function as_cmd(command)
+  return command_prefix .. command
+end
+
+local refactor_command_prefix = '<localleader>r'
+if vim.g.clojure_bindings_prefix then
+  refactor_command_prefix = vim.g.refactors_prefix
+end
+
+function as_refactor_cmd(command)
+  return refactor_command_prefix .. command
+end
+
